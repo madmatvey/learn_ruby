7b2919d704deadd88fbf5ebcd8988492298f61cb
madmatvey <potehin@gmail.com>
2015-12-19 19:11:19 +0400

    Simon Says

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,28 @@
+def echo (string)
+	string
+end
+
+def shout (string)
+	string.upcase
+end
+
+def repeat (string, count = 2)
+	((string + " ")*count).chop
+end
+
+def start_of_word (str, count)
+	str[0,count]
+end
+
+def first_word (str)
+	str.split(/[, \.?!]+/)[0]
+end
+
+def titleize (str)
+	result=str.split(/[, \.?!]+/)
+	if (result.size > 1) 
+		result.map! {|elem| elem.capitalize}
+	end
+# this don't work correcrly!
+	result.join(' ').capitalize
+end
